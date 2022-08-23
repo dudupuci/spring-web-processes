6b0072d1e3d9ed558b40413c6a61da8b69ecea6b
Eduardo Pucinelli <dudupucinelli@gmail.com>
2022-08-23 20:14:39 -0300

    Project created

---
+++ src/main/java/com/elotech/registrationprocesses/RegistrationProcessesApplication.java
@@ -0,0 +1,13 @@
+package com.elotech.registrationprocesses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationProcessesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationProcessesApplication.class, args);
+    }
+
+}
