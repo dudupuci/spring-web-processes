68d9323c4bc0d84cfe597566d73969a79e68b760
Eduardo Pucinelli <dudupucinelli@gmail.com>
2022-08-26 00:32:21 -0300

    Adding mock data and testing

--- src/main/java/com/elotech/registrationprocesses/configuration/ConfigTest.java
+++ src/main/java/com/elotech/registrationprocesses/configuration/ConfigTest.java
@@ -1,15 +1,41 @@
 package com.elotech.registrationprocesses.configuration;
 
+import com.elotech.registrationprocesses.entities.Pessoa;
+import com.elotech.registrationprocesses.entities.Processo;
+import com.elotech.registrationprocesses.entities.repositories.PessoaRepository;
+import com.elotech.registrationprocesses.entities.repositories.ProcessoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 @Profile("test")
 public class ConfigTest implements CommandLineRunner {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @Autowired
+    private ProcessoRepository processoRepository;
+
     @Override
     public void run(String... args) throws Exception {
+        List<Pessoa> list = new ArrayList<>();
+        Pessoa pessoa = new Pessoa(null, "Carlos Henrique", "14499445600", LocalDate.of(1996, 5, 9), LocalDate.now());
+        Pessoa pessoa2 = new Pessoa(null, "Bruno Fávaro", "06744189802", LocalDate.of(1995, 8, 22), LocalDate.now());
+        Pessoa pessoa3 = new Pessoa(null, "Marcelo Soares", "77455667800", LocalDate.of(1999, 1, 13), LocalDate.now());
+        list.addAll(Arrays.asList(pessoa, pessoa2, pessoa3));
+
+        Processo processo = new Processo(null,101010L,2022, LocalDate.now(),list);
+        processoRepository.save(processo);
+        pessoaRepository.saveAll(Arrays.asList(pessoa, pessoa2, pessoa3));
+
 
 
 
--- src/main/java/com/elotech/registrationprocesses/entities/controllers/PessoaController.java
+++ src/main/java/com/elotech/registrationprocesses/entities/controllers/PessoaController.java
@@ -1,9 +1,58 @@
 package com.elotech.registrationprocesses.entities.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.elotech.registrationprocesses.entities.Pessoa;
+import com.elotech.registrationprocesses.entities.services.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/pessoas")
 public class PessoaController {
+
+    @Autowired
+    private PessoaService pessoaService;
+
+    // get All
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<Pessoa>> findAll() {
+        List<Pessoa> list = pessoaService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    // get by id
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Pessoa> findById(@PathVariable Long id) {
+        Pessoa pessoa = pessoaService.findById(id);
+        return ResponseEntity.ok().body(pessoa);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Pessoa> insertPessoa(@RequestBody Pessoa pessoa) {
+        pessoa = pessoaService.insertPessoa(pessoa);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(pessoa.getId()).toUri();
+        return ResponseEntity.created(uri).body(pessoa);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Pessoa> updatePessoa(@PathVariable Long id, @RequestBody Pessoa pessoa) {
+        Pessoa oldPessoa = pessoaService.findById(id);
+        pessoaService.updateDataPessoa(pessoa, oldPessoa);
+        return ResponseEntity.ok().body(pessoa);
+    }
+
+
+    // delete by id
+    @RequestMapping(value = "/{id}", method = RequestMethod.
+            DELETE)
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        pessoaService.deletePessoa(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
